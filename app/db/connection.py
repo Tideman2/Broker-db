178b50016762f6a887cfb95a0055ba451d040fc4
Isaac Mark <isaacmark83@gmail.com>
2026-09-01 10:22:32 +0100

    chore: Set up production and dev environment and prepare for hosting server

--- app/db/connection.py
+++ app/db/connection.py
@@ -4,16 +4,30 @@ import mysql.connector
 
 load_dotenv()
 
+RUN_TEST = False if os.getenv("RUN_TEST") == "False" else True
+
 
 def get_connection():
     """To get db engine and connection."""
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        autocommit=False,
-        password=os.getenv('DB_CONNECTION_PASSWORD'),
-        database=os.getenv('DB_NAME'),
-    )
+
+    if (RUN_TEST):
+        return get_test_connection()
+
+    config = {
+        "host": os.getenv("PROD_DB_HOST", "localhost"),
+        "user": os.getenv("PROD_DB_USER", "root"),
+        "port": int(os.getenv("PROD_DB_PORT", "3306")),
+        "autocommit": False,
+        "password": os.getenv("PROD_DB_PASSWORD") or os.getenv("DB_CONNECTION_PASSWORD"),
+        "database": os.getenv("PROD_DB_NAME") or os.getenv("DB_NAME"),
+    }
+
+    ca_cert = os.getenv("PROD_DB_CA_CERT")
+
+    if ca_cert:
+        config["ssl_ca"] = ca_cert
+
+    return mysql.connector.connect(**config)
 
 
 def get_test_connection():
